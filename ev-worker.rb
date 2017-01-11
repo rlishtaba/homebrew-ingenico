390048b9f32b34202ee95e1df9f564891ccac4a7
Roman Lishtaba <roman@lishtaba.com>
2017-01-10 22:08:28 -0500

    releasing ev-worker v1.15.1

--- ev-worker.rb
+++ ev-worker.rb
@@ -1,9 +1,9 @@
 class EvWorker < Formula
   desc 'ev ci worker node'
   homepage 'http://artifactory.evf.us/artifactory'
-  url 'http://artifactory.evf.us/artifactory/automation-tools/bundles/ev-worker/1.14.1/package-170110003020.tar.gz'
-  sha256 '6cf8a2e366309a1cca78c3ce35e36ca419927426243e76a7a930cbaca2efb7e0'
-  version '1.14.1'
+  url 'http://artifactory.evf.us/artifactory/automation-tools/bundles/ev-worker/1.15.1/package-170110191521.tar.gz'
+  sha256 '42a63b8e1ad56acca0476d80ccc63f9140c5ec6a74dd5bce9dcc50b3407612c1'
+  version '1.15.1'
 
   def install
     ENV.deparallelize
@@ -50,8 +50,6 @@ class EvWorker < Formula
         <string>#{bin}/ev-worker</string>
         <string>-c</string>
         <string>#{etc}/ev-worker.yaml</string>
-        <string>-l</string>
-        <string>#{var}/log/ev-worker/access.log</string>
       </array>
       <key>RunAtLoad</key>
       <true/>
@@ -62,9 +60,9 @@ class EvWorker < Formula
       <key>Debug</key>
       <true/>
       <key>StandardErrorPath</key>
-      <string>#{var}/log/ev-worker/stderr.txt</string>
+      <string>#{var}/log/ev-worker/stderr.log</string>
       <key>StandardOutPath</key>
-      <string>#{var}/log/ev-worker/stdout.txt</string>
+      <string>#{var}/log/ev-worker/stdout.log</string>
     </dict>
     </plist>
   EOS
